aa9b7e53a19748b4c64a058997f097d0a50dc9e5
Kamyab78 <amirhosseinghassemi78@yahoo.com>
2022-01-27 18:51:26 +0330

    Add:shoting

--- Assets/script/onclickhandeler.cs
+++ Assets/script/onclickhandeler.cs
@@ -32,24 +32,27 @@ public class onclickhandeler : MonoBehaviour,IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        // GameObject go;
-        // go = Instantiate(bullet);
-        // go.transform.position = player.transform.position;
-        // // enemySpawArea.transform.position;
-        // go.transform.SetParent(parent.transform );
-        // go.transform.localScale = new Vector3(1, 1, 1);
-        // // Debug.Log(eventData.pressPosition);
+        GameObject go;
+        go = Instantiate(bullet);
+        go.transform.position = player.transform.position;
+        // enemySpawArea.transform.position;
+        go.transform.SetParent(parent.transform );
+        go.transform.localScale = new Vector3(1, 1, 1);
+        // Debug.Log(eventData.pressPosition);
         // Debug.Log(player.transform.position);
-        // // Debug.Log(player.transform.localPosition);
-        // Debug.Log(Input.mousePosition);
-        // // go.transform.localPosition.DOMove(new Vector3(0,0,0), 0.4f);
-        // go.transform.localPosition = (new Vector3(Input.mousePosition.x+player.transform.localPosition.x,Input.mousePosition.y+player.transform.localPosition.y,Input.mousePosition.z) );
+        Debug.Log(Input.mousePosition);
+        Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(temp);
+        // Debug.Log(player.transform.localPosition);
+        Debug.Log(Input.mousePosition);
+        go.transform.DOMove(temp, 0.5f);
+        // go.transform.position = temp;
         //
-        Vector3 shootDirection;
-        shootDirection = Input.mousePosition;
-        shootDirection.z = 0.0f;
-        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
-        shootDirection = shootDirection-transform.position;
+        // Vector3 shootDirection;
+        // shootDirection = Input.mousePosition;
+        // shootDirection.z = 0.0f;
+        // shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        // shootDirection = shootDirection-transform.position;
         //...instantiating the rocket
         // Rigidbody2D bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
         // bulletInstance.velocity = new Vector2(shootDirection.x * 0.2f, shootDirection.y * 0.2f);
